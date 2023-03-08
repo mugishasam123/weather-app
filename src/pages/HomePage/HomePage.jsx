58468fb81ddfd4acb0bc649a67d5802638d195ae
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-08 17:36:57 +0200

    remove unused codes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState} from 'react'
 import Search from '../../components/Search/Search'
 import CurrentWeather from '../../components/CurrentWeather/CurrentWeather'
 import Nav from '../../components/Nav/Nav'
