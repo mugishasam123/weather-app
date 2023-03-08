4d12766e0f6b2bb56fd5d130ce2b0047a1928666
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-08 17:28:42 +0200

    Add mockdata

---
+++ src/Data/mockData.jsx
@@ -0,0 +1,21 @@
+const mockData = {
+    city: 'Kigali',
+    weather: [
+      {
+        description: 'cold',
+        icon: '01d',
+      },
+    ],
+    main: {
+      temp: '20',
+      humidity: '10',
+      pressure: '100',
+      feels_like: '23',
+    },
+    wind: {
+      speed: '2',
+    },
+  }
+  
+  export default mockData
+  
