3ed0c546952f8ec7cfdeffa2194d5e69aca8a5d7
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-08 17:27:37 +0200

    Add api data

---
+++ src/Data/api.jsx
@@ -0,0 +1,4 @@
+export const GEO_API_URL = 'http://api.geonames.org'
+export const USERNAME = 'bertrandconxy'
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = '1ae041ae002793b6b54671f34bf6cde7'
